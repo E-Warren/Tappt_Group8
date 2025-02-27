5a480c349d239ba1e99ff8469eaf3a974599b260
Emily Warren <emilywarren056@gmail.com>
2025-02-27 03:49:15 -0600

    Implement login/signup with database connection
    2/27/25
    
    Native login system connected to PostgreSQL database hosted on AWS. All passwords are hashed using bcrypt. Input error handling before and after Express HTTP requests. This completes issue #17 and essentially completes feature #1 (although there may be some tweaking). Interface changes make the relationship between Google-assisted and native sign-ins much clearer (issue #19).
    
    Updated README to describe dependencies and instructions in more detail. Some minor aesthetics updates and some placeholder pages created.

---
+++ index.tsx
@@ -0,0 +1,33 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the home screen</Text>
+      <Link href={"/login"} style={styles.button}>
+        Go to login screen
+      </Link>
+      <Link href={"/slogin"} style={styles.button}>
+        Go to student login screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FC6A03",
+  },
+  text: {
+    color: '#fff'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
---
+++ view-decks.tsx
@@ -0,0 +1,28 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>View Decks</Text>
+      <Text style={styles.text}>Login/sign-up successful!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FC6A03",
+  },
+  text: {
+    color: '#fff'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
