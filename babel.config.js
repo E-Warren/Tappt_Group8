facfb056db11f94b4f117f816c7d55cfe253db36
Emily Warren <emilywarren056@gmail.com>
2025-04-30 15:32:22 -0500

    Testing, README, & some FE changes
    4/30/25: Added unit test using jest and created a production-focused README update. Rewired logout button on view-decks and removed the logout button from sign-in/up. Adjusted a few other frontend elements for size and similar things.
    
    The last thing left is Googe API!

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+  module.exports = function (api) {
+    api.cache(true);
+    return {
+      presets: ['@babel/preset-env', 'babel-preset-expo', '@babel/preset-typescript'],
+      plugins: [
+        ['@babel/plugin-transform-class-properties', { loose: true }],
+        ['@babel/plugin-transform-private-methods', { loose: true }],
+        ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+        ['@babel/plugin-transform-runtime', { loose: true, regenerator: true }],
+      ],
+    };
+  };
+  
