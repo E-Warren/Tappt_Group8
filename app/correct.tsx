b10f8d14d4ca2595f2b359eab4416eec52f24990
sy1100 <sierrayang12@gmail.com>
2025-04-29 23:04:50 -0500

    Update correct.tsx

--- app/correct.tsx
+++ app/correct.tsx
@@ -1,4 +1,4 @@
- import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useStudentStore } from "./useWebSocketStore";
